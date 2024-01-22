7626a89c3ffd66d933231a00bda7d0b38542c0c0
Rayane Larkem <89400140+RayaneLark@users.noreply.github.com>
2024-01-23 00:16:46 +0100

    exo2 from q1 to q2

--- exercice2.r
+++ exercice2.r
@@ -1,30 +1,22 @@
-# Installer et charger les bibliothèques nécessaires
-library(ggplot2)
-
 # Charger les données
-performance_2012 <- read.table("company2012.dat", header = FALSE)
-performance_2017 <- read.table("company2017.dat", header = FALSE)
-performance_2022 <- read.table("company2022.dat", header = FALSE)
+data_2012 <- read.table("company2012.dat", header = FALSE)
+data_2017 <- read.table("company2017.dat", header = FALSE)
+data_2022 <- read.table("company2022.dat", header = FALSE)
 
-# Créer des graphiques pour chaque année avec des estimations de densité de noyau
-ggplot() +
-  # Histogramme et densité pour 2012
-  geom_histogram(aes(x = performance_2012$Performance), bins = 30, alpha = 0.5, fill = "blue", color = "black") +
-  geom_density(aes(x = performance_2012$Performance), color = "blue") +
-  
-  # Histogramme et densité pour 2017
-  geom_histogram(aes(x = performance_2017$Performance), bins = 30, alpha = 0.5, fill = "green", color = "black") +
-  geom_density(aes(x = performance_2017$Performance), color = "green") +
-  
-  # Histogramme et densité pour 2022
-  geom_histogram(aes(x = performance_2022$Performance), bins = 30, alpha = 0.5, fill = "red", color = "black") +
-  geom_density(aes(x = performance_2022$Performance), color = "red") +
-  
-  # Personnaliser les étiquettes et le titre
-  labs(title = "Distribution des performances par année",
-       x = "Performance",
-       y = "Fréquence") +
-  
-  # Modifier les couleurs des légendes
-  scale_fill_manual(values = c("blue", "green", "red")) +
-  scale_color_manual(values = c("blue", "green", "red"))
+# Nommer les colonnes si nécessaire (à ajuster en fonction de vos données)
+colnames(data_2012) <- c("performances")
+colnames(data_2017) <- c("performances")
+colnames(data_2022) <- c("performances")
+
+# Créer un histogramme pour chaque année et les superposer
+hist(data_2012$performances, col = "blue", main = "Distribution des performances", xlab = "Performances", ylab = "Densité", probability = TRUE)
+hist(data_2017$performances, col = "green", add = TRUE, probability = TRUE)
+hist(data_2022$performances, col = "red", add = TRUE, probability = TRUE)
+
+# Ajouter les courbes de densité correspondantes
+lines(density(data_2012$performances), col = "blue", lwd = 2)
+lines(density(data_2017$performances), col = "green", lwd = 2)
+lines(density(data_2022$performances), col = "red", lwd = 2)
+
+# Légende
+legend("topright", legend = c("2012", "2017", "2022"), fill = c("blue", "green", "red"))
